458bb4b6f49063c375b0e643a3c2ce1002e5bbd0
nurhabibrs <49638644+nurhabibrs@users.noreply.github.com>
2021-01-12 23:15:18 +0700

    Add Array, Perulangan, and Percabangan

---
+++ Percabangan/src/com/dicoding/javafundamental/percabangan/ifThen.java
@@ -0,0 +1,10 @@
+package com.dicoding.javafundamental.percabangan;
+
+public class ifThen {
+    public static void main(String[] args) {
+        boolean isOn = true;
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        }
+    }
+}
---
+++ Percabangan/src/com/dicoding/javafundamental/percabangan/ifThenElse.java
@@ -0,0 +1,12 @@
+package com.dicoding.javafundamental.percabangan;
+
+public class ifThenElse {
+    public static void main(String[] args) {
+        boolean isOn = false;
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        } else {
+            System.out.println("Kondisi tidak terpenuhi...");
+        }
+    }
+}
